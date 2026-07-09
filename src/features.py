f288abce554e47fffe5c8e536a7d2608794d4cb0
gry2109 <geriley1@sheffield.ac.uk>
2026-07-09 14:53:02 +0100

    Some headings missing from datasets, others with differing column names between sets. Added simple if loop to skip over these for now.

--- src/features.py
+++ src/features.py
@@ -21,7 +21,13 @@ def feature_extraction(file_path, condition_label):
     # Define list of sensors we want to extract features from
     sensor_list = ['PlateHFAccZ', 'SpindleAccX', 'SpindleAccY', 'SpindleAccZ', 'Power']
     extracted_features = []
-    num_runs = len(dataset['PlateHFAccZ'])
+    
+    
+    
+    # Don't all have same heading names for the datasets, need to check keys in dataset
+    available_sensors = list(dataset.keys())
+    first_sensor = available_sensors[0]  # Get the first sensor to determine the number of runs
+    num_runs = len(dataset[first_sensor])  # Assuming all sensors have the same number of runs
     
     # Extraction loop for each sensor
     for i in range(num_runs):
@@ -29,15 +35,21 @@ def feature_extraction(file_path, condition_label):
             'Run_Number': i + 1,
             'Target_Condition': condition_label
         }
-        
+        #add check to see if the sensor is in the available sensors, if not skip it
         for sensor in sensor_list:
-            wave = dataset[sensor][i]
-            run_features[f'{sensor}_Mean'] = np.mean(wave)
-            run_features[f'{sensor}_RMS'] = np.sqrt(np.mean(wave**2))
-            run_features[f'{sensor}_Max_Peak'] = np.max(np.abs(wave))
-            run_features[f'{sensor}_Skewness'] = skew(wave)
-            run_features[f'{sensor}_Kurtosis'] = kurtosis(wave)
-            
+            if sensor in available_sensors:
+                wave = dataset[sensor][i]
+                run_features[f'{sensor}_Mean'] = np.mean(wave)
+                run_features[f'{sensor}_RMS'] = np.sqrt(np.mean(wave**2))
+                run_features[f'{sensor}_Max_Peak'] = np.max(np.abs(wave))
+                run_features[f'{sensor}_Skewness'] = skew(wave)
+                run_features[f'{sensor}_Kurtosis'] = kurtosis(wave)
+            else:
+                run_features[f'{sensor}_Mean'] = np.nan
+                run_features[f'{sensor}_RMS'] = np.nan
+                run_features[f'{sensor}_Max_Peak'] = np.nan
+                run_features[f'{sensor}_Skewness'] = np.nan
+                run_features[f'{sensor}_Kurtosis'] = np.nan
         extracted_features.append(run_features)
         
     return pd.DataFrame(extracted_features)
