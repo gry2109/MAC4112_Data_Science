890894b87d610bfb26249800fcd994ff9558f8aa
gry2109 <geriley1@sheffield.ac.uk>
2026-07-13 15:56:09 +0100

    Amended code calling on feature extraction functio to make more readable, added code to call on the clean_data_only function

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import os
 import pandas as pd
 from src.features import feature_extraction
+from src.clean_data import clean_data_only
 
-#So far chekcing this like whta was done in the explore notebook, now trying in main block of code for two datasets
-# used these two as they are the smallest and largest datasets, so we can see if the code works for both small and large datasets
+
+
 def main():
+    """
+    Main function to orchestrate the feature extraction process.
+    improve description of what this does. 
+    """
+    ### Feature extraction process ###
     print("Starting feature extraction process...")
-    # 1. Process a Healthy Baseline file
-    df_baseline = feature_extraction('data/Segmented_Linear_Baseline.mat', 'Baseline')      # this is what should be a command line interface to choose whtehr you want linear, machining etc. 
-    
-    # 2. Process a Faulty file (Let's use Misalignment as an example)
+    # 1. Process the files to conduct the aalysis on
+    df_baseline = feature_extraction('data/Segmented_Linear_Baseline.mat', 'Baseline')      # this is what should be a command line interface to choose whtehr you want linear, machining etc.
     df_heavy = feature_extraction('data/Segmented_Linear_Heavy.mat', 'Heavy')
-
     df_override = feature_extraction('data/Segmented_Linear_Override.mat', 'Override')
-    
-    # 3. Combine them into one master dataset
+
+    # 2. Combine them into one master dataset
     print("Combining datasets...")
     master_dataset = pd.concat([df_baseline, df_heavy, df_override], ignore_index=True)
-    
-    # 4. Save the final table as a CSV so we can easily look at it
+
+    # 3. Save the final table as a CSV so we can easily look at it
     output_path = 'results/master_features.csv'
     master_dataset.to_csv(output_path, index=False)
-    
-    print(f" Success! Master dataset saved to {output_path}")
+    print(f"Success! Master dataset saved to {output_path}")
     print(f"Total rows: {len(master_dataset)} | Total columns: {len(master_dataset.columns)}")
+
+    ### Cleaning process ###
+    print("Starting data cleaning process...")
+
+    df_cleaned = clean_data_only(master_dataset)
+    df_cleaned.to_csv('results/master_features_cleaned.csv', index=False)
+    print(f"Data cleaning complete! Cleaned dataset saved to 'results/master_features_cleaned.csv'")
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
