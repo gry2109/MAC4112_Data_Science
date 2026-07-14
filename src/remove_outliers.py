9538a4c548e6661781f3ddfb0c42b9815693b6bd
gry2109 <geriley1@sheffield.ac.uk>
2026-07-14 11:21:45 +0100

    Fixed typo in df_clean

--- src/remove_outliers.py
+++ src/remove_outliers.py
@@ -8,6 +8,6 @@ def remove_outliers(df, feature_cols, contamination=0.02):
     iso = IsolationForest(contamination=contamination, random_state = 42)
     outlier_predictions = iso.fit_predict(df[feature_cols])
     df_clean = df[outlier_predictions ==1].reset_index(drop=True)
-    dropped_count = len(df)-len(df_celan)
+    dropped_count = len(df)-len(df_clean)
     print(f"Outlier rejection complete: Dropped {dropped_count} anamolous runs.")
     return df_clean
