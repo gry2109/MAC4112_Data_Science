51909929705605668af26b5010ce28c386ea66c8
gry2109 <geriley1@sheffield.ac.uk>
2026-07-14 11:10:36 +0100

    created function to remove outliers

---
+++ src/remove_outliers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+
+def remove_outliers(df, feature_cols, contamination=0.02):
+    """
+    
+    """
+    iso = IsolationForest(contamination=contamination, random_state = 42)
+    outlier_predictions = iso.fit_predict(df[feature_cols])
+    df_clean = df[outlier_predictions ==1].reset_index(drop=True)
+    dropped_count = len(df)-len(df_celan)
+    print(f"Outlier rejection complete: Dropped {dropped_count} anamolous runs.")
+    return df_clean
